0fc42c157c277937a71a998935cbf8fea6152298
Josh Bruce <josh@8fold.pro>
2021-10-24 10:00:00 -0500

    refactor the 500 server response to remove duplication

--- public/index.php
+++ public/index.php
@@ -8,19 +8,20 @@ require __DIR__ . '/../vendor/autoload.php';
 
 Dotenv\Dotenv::createImmutable(__DIR__ . '/../')->load();
 
-// -> 500 server code dotenv required variables not set
-$envHasRequired = array_key_exists('CONTENT_UP', $_SERVER) and
-    array_key_exists('CONTENT_FOLDER', $_SERVER);
+function emitErrorHeaders(int $code): void {
+    if ($code === 500) {
+        header(
+            'HTTP/2 500 Internal server error',
+            replace: true,
+            response_code: 500
+        );
+        header('Cache-Control: no-cache, must-revalidate');
+        // dont' need text/hemlt header, default seems fine
 
-if (! $envHasRequired) {
-    header(
-        'HTTP/2 500 Internal server error',
-        replace: true,
-        response_code: 500
-    );
-    header('Cache-Control: no-cache, must-revalidate');
-    // dont' need text/hemlt header, default seems fine
+    }
+}
 
+function emitErrorBody(int $code, string $details): void {
     print <<<html
         <!doctype html>
         <html>
@@ -28,7 +29,7 @@ if (! $envHasRequired) {
                 <title>Server error | Josh Bruce's personal site</title>
             </head>
             <body>
-                <h1>500: Internal server error (environment)</h1>
+                <h1>$code: Internal server error ($details)</h1>
                 <p>We're not sure what happened here. Please try again later.</p>
                 <p>If this error persists, please contact <a href="https://github.com/joshbruce">Josh Bruce</a>.</p>
             </body>
@@ -38,33 +39,21 @@ if (! $envHasRequired) {
     exit;
 }
 
+// -> 500 server code dotenv required variables not set
+$envHasRequired = array_key_exists('CONTENT_UP', $_SERVER) and
+    array_key_exists('CONTENT_FOLDER', $_SERVER);
+
+if (! $envHasRequired) {
+    emitErrorHeaders(500);
+    emitErrorBody(500, 'environment');
+}
+
 // -> 500 server code no content connection possible
 $contentRoot = __DIR__ . '/content-dir';
 
 $contentExists = file_exists($contentRoot) and is_dir($contentRoot);
 
 if (! $contentExists) {
-    header(
-        'HTTP/2 500 Internal server error',
-        replace: true,
-        response_code: 500
-    );
-    header('Cache-Control: no-cache, must-revalidate');
-    // dont' need text/hemlt header, default seems fine
-
-    print <<<html
-        <!doctype html>
-        <html>
-            <head>
-                <title>Server error | Josh Bruce's personal site</title>
-            </head>
-            <body>
-                <h1>500: Internal server error (content)</h1>
-                <p>We're not sure what happened here. Please try again later.</p>
-                <p>If this error persists, please contact <a href="https://github.com/joshbruce">Josh Bruce</a>.</p>
-            </body>
-        </html>
-    html;
-
-    exit;
+    emitErrorHeaders(500);
+    emitErrorBody(500, 'content');
 }
