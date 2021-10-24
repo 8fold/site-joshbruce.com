30bdb4d3db188a9ecc700f1d687f497dfc4831f9
Josh Bruce <josh@8fold.pro>
2021-10-24 09:46:31 -0500

    500 error has page

--- public/index.php
+++ public/index.php
@@ -13,7 +13,20 @@ if (! $contentExists) {
         replace: true,
         response_code: 500
     );
+    header('Cache-Control: no-cache, must-revalidate');
     // dont' need text/hemlt header, default seems fine
 
-    print 'Internal server error';
+    print <<<html
+        <!doctype html>
+        <html>
+            <head>
+                <title>Server error | Josh Bruce's personal site</title>
+            </head>
+            <body>
+                <h1>500: Internal server error</h1>
+                <p>We're not sure what happened here. Please try again later.</p>
+                <p>If this error persists, please contact <a href="https://github.com/joshbruce">Josh Bruce</a>.</p>
+            </body>
+        </html>
+    html;
 }
