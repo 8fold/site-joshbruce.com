6a17ad55c52d84656178e7f391b4435fd47c2747
Josh Bruce <josh@8fold.pro>
2021-10-26 15:25:26 -0500

    Update AppTest.php

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -12,13 +12,13 @@ test('Content can be in sub-folder', function() {
     $elapsed = $endTime - $startTime;
     $ms      = $elapsed/1e+6;
 
-    expect($ms)->toBeLessThan(191); // used to be 29ms
+    expect($ms)->toBeLessThan(225); // used to be 29ms
 
     expect(
         $body
     )->toBe(<<<html
         <!doctype html>
-        <html lang="en"><head><title>sub-folder | Josh Bruce's personal site</title><meta charset="utf-8"><link rel="stylesheet" href="/css/main.css"></link></head><body><h1>A sub-folder content</h1><p>This content was successfully found.</p></body></html>
+        <html lang="en"><head><title>sub-folder | Josh Bruce's personal site</title><meta charset="utf-8"><link rel="stylesheet" href="/css/main.css"></link><link type="image/x-icon" rel="icon" href="/assets/favicons/favicon.ico"></link><link rel="apple-touch-icon" href="/assets/favicons/apple-touch-icon.png" sizes="180x180"></link><link rel="image/png" href="/assets/favicons/favicon-32x32.png" sizes="32x32"></link><link rel="image/png" href="/assets/favicons/favicon-16x16.png" sizes="16x16"></link></head><body><h1>A sub-folder content</h1><p>This content was successfully found.</p></body></html>
         html
     );
 });
@@ -58,7 +58,7 @@ test('Content is from file system', function() {
         $body
     )->toBe(<<<html
         <!doctype html>
-        <html lang="en"><head><title>Not found</title><meta charset="utf-8"><link rel="stylesheet" href="/css/main.css"></link></head><body><h1>404: Not found</h1><p>We still haven’t found what you’re looking for.</p></body></html>
+        <html lang="en"><head><title>Not found</title><meta charset="utf-8"><link rel="stylesheet" href="/css/main.css"></link><link type="image/x-icon" rel="icon" href="/assets/favicons/favicon.ico"></link><link rel="apple-touch-icon" href="/assets/favicons/apple-touch-icon.png" sizes="180x180"></link><link rel="image/png" href="/assets/favicons/favicon-32x32.png" sizes="32x32"></link><link rel="image/png" href="/assets/favicons/favicon-16x16.png" sizes="16x16"></link></head><body><h1>404: Not found</h1><p>We still haven’t found what you’re looking for.</p></body></html>
         html
     );
 });
