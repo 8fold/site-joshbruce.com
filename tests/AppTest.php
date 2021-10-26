5fa8ba0d5c9f87a8929ac534e5645ddad57a0236
Josh Bruce <josh@8fold.pro>
2021-10-26 06:52:08 -0500

    Update AppTest.php

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -12,7 +12,7 @@ test('Content can be in sub-folder', function() {
     $elapsed = $endTime - $startTime;
     $ms      = $elapsed/1e+6;
 
-    expect($ms)->toBeLessThan(27);
+    expect($ms)->toBeLessThan(28);
 
     expect(
         $body
