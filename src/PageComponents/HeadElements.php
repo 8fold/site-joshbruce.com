6c83e6acb3543ab115a1f50f03410289efafdd3d
Josh Bruce <josh@8fold.pro>
2021-10-30 20:58:25 -0500

    Update HeadElements.php

--- src/PageComponents/HeadElements.php
+++ src/PageComponents/HeadElements.php
@@ -15,7 +15,13 @@ class HeadElements
      */
     public static function create(): array
     {
-        $headElements   = Favicons::create();
+        $headElements   = [
+            HtmlElement::meta()
+                ->props('name viewport', 'content width=device-width,initial-scale=1')
+        ];
+
+        $headElements = array_merge($headElements, Favicons::create());
+
         $headElements[] = HtmlElement::link()
             ->props('rel stylesheet', 'href /assets/css/main.min.css');
         return $headElements;
