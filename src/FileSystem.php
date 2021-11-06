a2000a74d2e154b23d2e1da94508cdf8339e41e9
Josh Bruce <josh@8fold.pro>
2021-11-06 12:52:30 -0500

    Move content to project root

--- src/FileSystem.php
+++ src/FileSystem.php
@@ -9,7 +9,7 @@ use DirectoryIterator;
 use JoshBruce\Site\Content\FrontMatter;
 
 /**
- * @todo: Change contentRoot to be the folder in which the text-based content is.
+ * @todo: Remove contentRoot by moving content to project root.
  */
 class FileSystem
 {
