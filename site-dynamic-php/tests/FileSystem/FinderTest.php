fd8eb680f404d47da0d66aa207f4078ef7892cad
Josh Bruce <josh@8fold.pro>
2022-03-15 11:27:47 -0400

    tests

--- site-dynamic-php/tests/FileSystem/FinderTest.php
+++ site-dynamic-php/tests/FileSystem/FinderTest.php
@@ -12,9 +12,9 @@ use JoshBruce\SiteDynamic\Environment;
 
 final class FinderTest extends LiveContentTestCase
 {
-    private const PUBLISHED_COUNT = 55;
+    private const PUBLISHED_COUNT = 56;
 
-    private const DRAFT_COUNT = 12;
+    private const DRAFT_COUNT = 11;
 
     /**
      * @test
