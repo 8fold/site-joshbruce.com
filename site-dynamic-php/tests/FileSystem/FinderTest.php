96db019890519034ebae1450025338017df54c2b
Josh Bruce <josh@8fold.pro>
2022-04-22 06:14:01 -0400

    fix: published count

--- site-dynamic-php/tests/FileSystem/FinderTest.php
+++ site-dynamic-php/tests/FileSystem/FinderTest.php
@@ -12,7 +12,7 @@ use JoshBruce\SiteDynamic\Environment;
 
 final class FinderTest extends LiveContentTestCase
 {
-    private const PUBLISHED_COUNT = 65;
+    private const PUBLISHED_COUNT = 66;
 
     private const DRAFT_COUNT = 1;
 
