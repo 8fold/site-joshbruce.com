a0e4add63e1eeea26a80e0cd933062213d937ce9
Josh Bruce <josh@8fold.pro>
2022-07-31 18:57:17 -0400

    Update index.php

--- site-root/public/index.php
+++ site-root/public/index.php
@@ -13,52 +13,6 @@ require __DIR__ . '/../../vendor/autoload.php';
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 
 use Eightfold\Amos\Site;
-use Eightfold\Amos\Content;
-
-use Nyholm\Psr7Server\ServerRequestCreator;
-use Nyholm\Psr7\Factory\Psr17Factory;
-
-use FE\Templates\Page;
-use FE\Templates\PageNotFound;
-
-$psr17Factory = new Psr17Factory();
-
-$request = (new ServerRequestCreator(
-    $psr17Factory, // ServerRequestFactory
-    $psr17Factory, // UriFactory
-    $psr17Factory, // UploadedFileFactory
-    $psr17Factory  // StreamFactory
-))->fromGlobals();
-
-(new SapiEmitter())->emit(
-    Site::init(
-        withDomain: 'http://4th.earth',
-        contentIn: Content::init(__DIR__ . '/../../content-root')
-    )->templates(
-        default: Page::class,
-        templates: [
-            'error404' => PageNotFound::class
-        ]
-    )->response(for: $request)
-);
-exit();
-
-<?php
-declare(strict_types=1);
-
-ini_set('display_errors', '0');
-ini_set('display_startup_errors', '0');
-error_reporting(E_ALL);
-
-ini_set('realpath_cache_size', '4096');
-ini_set('realpath_cache_ttl', '600');
-
-require __DIR__ . '/../../vendor/autoload.php';
-
-use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
-
-use Eightfold\Amos\Site;
-use Eightfold\Amos\Content;
 
 use Nyholm\Psr7Server\ServerRequestCreator;
 use Nyholm\Psr7\Factory\Psr17Factory;
@@ -77,9 +31,9 @@ $request = (new ServerRequestCreator(
 
 (new SapiEmitter())->emit(
     Site::init(
-        withDomain: 'https://joshbruce.com',
-        contentIn: Content::init(__DIR__ . '/../../content-root')
-    )->templates(
+        withDomain: 'http://joshbruce.com',
+        contentIn: __DIR__ . '/../../content-root'
+    )->setTemplates(
         default: Page::class,
         templates: [
             'error404' => PageNotFound::class
@@ -87,4 +41,3 @@ $request = (new ServerRequestCreator(
     )->response(for: $request)
 );
 exit();
-
