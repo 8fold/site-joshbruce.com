af52b3a39f5fc3c4a6cd06d1f20e73c743082e7d
Josh Bruce <josh@8fold.pro>
2022-07-31 09:29:25 -0400

    update: Log list to use Amos

---
+++ src/Partials/LogList.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace JoshBruce\Site\Partials;
+
+use Eightfold\XMLBuilder\Contracts\Buildable;
+
+use Eightfold\Amos\Site;
+
+use Eightfold\HTMLBuilder\Element;
+
+class LogList
+{
+    public static function create(Site $site): self
+    {
+        return new self($site);
+    }
+
+    final private function __construct(private Site $site)
+    {
+    }
+
+    private function site(): Site
+    {
+        return $this->site;
+    }
+
+    public function build(): string
+    {
+       $path = $this->site()->publicRoot() . $this->site()->requestPath();
+        if (is_dir($path) === false) {
+            return '';
+        }
+
+        $contents = scandir($path);
+        $links = [];
+        foreach ($contents as $content) {
+            if (
+                $content === '.' or
+                $content === '..' or
+                $content === '.DS_Store' or
+                str_contains($content, '.')
+            ) {
+                continue;
+            }
+
+            $href = $this->site()->requestPath() . '/' . $content;
+
+            $meta = $this->site()->meta($href);
+            if (property_exists($meta, 'title')) {
+                $index = intval($content);
+                $links[$index] = Element::li(
+                    Element::a($meta->title)->props('href ' . $href)
+                );
+            }
+        }
+
+        krsort($links);
+
+        return Element::ul(...$links)->build();
+    }
+
+    public function __toString(): string
+    {
+        return $this->build();
+    }
+}
